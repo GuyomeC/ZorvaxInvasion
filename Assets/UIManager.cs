51659b94f1dd72ba23e5b62d0a3a0e7d9400abbe
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2024-05-15 22:48:44 +0200

    update ui manager

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class UIManager : MonoBehaviour
 {
@@ -50,6 +53,11 @@ public class UIManager : MonoBehaviour
         pausePanel.SetActive(true);
         Time.timeScale = 0;
 
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void Continue()
