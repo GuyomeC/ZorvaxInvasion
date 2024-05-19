60eb942f57d9ed8b6ad447fa24351c4a73290b66
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2024-05-19 22:18:34 +0200

    update perk

--- Assets/Script/PerkShop.cs
+++ Assets/Script/PerkShop.cs
@@ -7,18 +7,6 @@ public class PerkShop : MonoBehaviour
     [Header("Entity")]
     [SerializeField] private HeroEntity _entity;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void BuyDash()
     {
         if (_entity.haveDash == false && MoneyManager.instance.currentCores >= 3)
